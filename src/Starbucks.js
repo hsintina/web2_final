9d83592f52eeaf1d3d23614004e1363e8cd3d6e0
hsintina <hsintingting@gmail.com>
2020-04-27 20:22:34 -0400

    test2

--- src/Starbucks.js
+++ src/Starbucks.js
@@ -8,13 +8,15 @@ function Starbucks() {
     <div>
        <Nav />
        <div className="section1">
-       <div className="worktitle">
-      <h1 >Starbucks</h1>
-      <p>Global Branding Promotion</p>
-      </div>
-      <div ><img className="banner" src={banner} alt="starbucks" /></div>
+          <div className="worktitle">
+            <h1>Starbucks</h1>
+            <p>Global Branding Promotion</p>
+          </div>
+      <div>
+        <img className="banner" src={banner} alt="starbucks" />
      </div>
-    </div>
+     </div>
+     </div>
   );
 }
 
