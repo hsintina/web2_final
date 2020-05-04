fd2866d6274e608c88feb019f2a717768be8c506
hsintina <hsintingting@gmail.com>
2020-05-03 21:45:02 -0400

    second try

--- src/About.js
+++ src/About.js
@@ -12,7 +12,7 @@ function About() {
       <br/>
       <p className="intro">I am an illustration-based motion designer who dedicate to creating dynamic pieces with subtle illustration. 
         I strive to combine 3D concept into 2D work. 
-        Besides, I am also interested in photography in my free time.</p>
+        Besides, I am interested in photography in my free time.</p>
         <div><Footer/></div>
     </div>
   );
